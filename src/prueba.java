3b0e6ea96db71326666b15673e40fb2c3feceecf
jesusususususu <jesusbenitogonzalez1234@gmail.com>
2024-05-22 10:51:43 +0200

    añadir rama funcionalidad

--- src/prueba.java
+++ src/prueba.java
@@ -1,5 +1,12 @@
 
 public class prueba {
 	
+	public static void main(String []args) {
+		System.out.println("hola");
+		System.out.println("adios");
+		System.out.println("adios rama");
+		System.out.println("añado rama nueva funcionalidad");
 
+	}
+	
 }
