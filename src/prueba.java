57c7d5e18f6eea9b8c589054116b577163f6dac0
jesusususususu <jesusbenitogonzalez1234@gmail.com>
2024-05-22 10:59:16 +0200

    nuevo contenido desde master

--- src/prueba.java
+++ src/prueba.java
@@ -6,7 +6,7 @@ public class prueba {
 		System.out.println("adios");
 		System.out.println("adios rama");
 		System.out.println("añado rama nueva funcionalidad");
-
+		System.out.println("nuevo desde master");
 	}
 	
 }
